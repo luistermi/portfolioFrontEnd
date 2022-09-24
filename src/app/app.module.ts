adaca496b0edbafa8461a0b2b69bd4e8dcfb0b70
luistermi <luis.termi.i@gmail.com>
2022-09-24 17:20:16 -0300

    Avances con componente educacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
@@ -22,7 +23,8 @@ import { ProyectosComponent } from './componentes/principal/proyectos/proyectos.
     ProyectosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/principal/educacion/educacion.component.ts
+++ src/app/componentes/principal/educacion/educacion.component.ts
@@ -12,4 +12,44 @@ export class EducacionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  formaciones = [
+  {anio: "2021-Actualidad", titulo: "#SeProgramar y #YoProgramo. Argentina Programa. Secretaría de Economía del Conocimiento."},
+  {anio: "2001-Actualidad", titulo: "Licenciatura en Economía. Facultad de Ciencias Económicas. Universidad de Buenos Aires."},
+  {anio: "2000", titulo: "Bachiller. Instituto Superior Martín Güemes. Berazategui."}];
+  
+  editable = false;
+  addable = false;
+  visible=true;
+  contadorAddable: number[]= [];
+  formacionAnio = "";
+  formacionTitulo = "";
+  
+  toEditable(){
+  this.editable = true;
+  }
+
+  toNotEditable(){
+    this.editable = false;
+    this.contadorAddable = [];
+    this.addable = false;
+    if(this.formacionAnio!="" || this.formacionTitulo!=""){
+    this.formaciones.push({anio:this.formacionAnio,titulo:this.formacionTitulo})
+    }
+    this.formacionAnio = "";
+    this.formacionTitulo = "";
+    this.visible=true;
+  }
+  
+  clean(index: number): void {
+    this.formaciones.splice(index, 1);
+  }
+  
+  add(){
+    this.addable = true;
+    this.contadorAddable.push(1);
+  }
+  
+  visibleOff(){
+  this.visible=false;
+  }
 }
