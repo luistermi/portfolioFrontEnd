1858de186d8fcf0a853adaa932e3f0097b04c193
luistermi <luis.termi.i@gmail.com>
2022-09-24 23:43:49 -0300

    Desarrollo de la componente acerca-de

--- src/app/componentes/principal/acerca-de/acerca-de.component.ts
+++ src/app/componentes/principal/acerca-de/acerca-de.component.ts
@@ -12,4 +12,17 @@ export class AcercaDeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  nombreApellido = "Luis Salvador Augusto Terminiello";
+  profesion = "Desarrollador Full Stack Web Junior";
+  presentacionPersonal = "Soy estudiante avanzado de la Licenciatura en Economía en la UBA. Tengo experiencia de más de diez años trabajando en área de QA de la Dirección General de Sistemas de la Administración Financiera, principalmente en testing funcional y en los últimos años en automation.";
+
+    editable = false;
+
+    toEditable(){
+    this.editable = true;
+    }
+  
+    toNotEditable(){
+      this.editable = false;
+        }
 }
